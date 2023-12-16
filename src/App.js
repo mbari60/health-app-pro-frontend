a2f86d27430040d4253a3411a3fa1e9b081dcca7
kevin <kevinmbari600@gmail.com>
2023-12-16 17:49:42 +0300

    patientform

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages.js/home";
 import Footer from "./Components/Footer";
+import "react-toastify/dist/ReactToastify.css";
+import PatientForm from "./Pages.js/PatientForm";
 
 function App() {
   return (
@@ -9,8 +11,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/PatientForm" element={<PatientForm />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </main>
   );
 }
